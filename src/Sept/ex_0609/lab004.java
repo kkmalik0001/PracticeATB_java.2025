20acdf49adeb5a5fe362d35f112159bdd2582ba3
Your Name <you@example.com>
2025-10-13 08:51:54 +0530

    practice ATB 06 OCT

---
+++ src/Sept/ex_0609/lab004.java
@@ -0,0 +1,20 @@
+package Sept.ex_0609;
+
+public class lab004     {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2+2);
+    }
+
+    // package: Nothing just a java folder, where is your class or code is present
+    // syntax: package name_of_package.
+    //java : OOPS CONCEPTS FOLLOW.
+    // structure of java program : Everything will be within class
+    /*
+    Class - Blueprint
+    class name {
+    main{
+    }
+    }
+     */
+}
---
+++ src/Sept/ex_0609/lab007.java
@@ -0,0 +1,15 @@
+package Sept.ex_0609;
+
+public class lab007 {
+    public static void main(String[] kapil) {
+        System.out.println("hi, i am testing that args is just a reference name and can use anything");
+
+   //Single line comment with double
+        /*
+        * double line comment
+        * */
+
+//        System.out.println();
+        
+    }
+}
